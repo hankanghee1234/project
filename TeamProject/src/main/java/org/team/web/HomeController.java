eba2c661a010cf89c650e2d5b8a7a81951d580ff
ss <ss@hankanghee>
2017-01-17 15:57:15 +0900

    kljhjkj

---
+++ TeamProject/src/main/java/org/team/web/HomeController.java
@@ -0,0 +1,64 @@
+package org.team.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/main/*")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public void Test() throws Exception {
+		logger.info("TEST............");
+	}
+	
+	@RequestMapping(value="/impress", method = RequestMethod.GET)
+	public void impressTest() throws Exception {
+		logger.info("impress TEST............");
+	}
+	
+	@RequestMapping(value="/reveal", method = RequestMethod.GET)
+	public void revealTest() throws Exception {
+		logger.info("reveal TEST............");
+	}
+	
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public void index() throws Exception {
+		logger.info("index TEST............");
+	}
+	
+	@RequestMapping(value="/register", method = RequestMethod.GET)
+	public void registerTest() throws Exception {
+		logger.info("register TEST............");
+	}
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
