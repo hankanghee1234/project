746d71911a082ac945d8c42f2eeca8d0acb84950
ss <ss@hankanghee>
2017-01-17 17:20:11 +0900

    DAO TEST

--- TeamProject/src/test/java/org/team/web/MemberDAOTest.java
+++ TeamProject/src/test/java/org/team/web/MemberDAOTest.java
@@ -2,6 +2,8 @@ package org.team.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,21 +27,41 @@ public class MemberDAOTest {
 		vo.setUname("user10");
 		
 		dao.create(vo);
+		System.out.println(vo);
+		
 	}
 	
 	@Test
 	public void testRead() throws Exception {
 		
+		String userid = "user00";
+		System.out.println(dao.read(userid));
 	}
 	
 	@Test
 	public void testUpdate() throws Exception {
 		
+		MemberVO vo = new MemberVO();
+		vo.setUserpw("user11");
+		vo.setEmail("bbb@ccc.com");
+		vo.setUname("user11");
+		vo.setUserid("user00");
+		
+		dao.update(vo);
+		System.out.println(vo);
 	}
 	
 	@Test
 	public void testDelete() throws Exception {
 		
+		String userid = "user10";
+		dao.delete(userid);
+		System.out.println(userid);
 	}
-
+	
+	@Test
+	public void testList() throws Exception {
+		
+		System.out.println(dao.list());
+	}
 }
