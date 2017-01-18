f1afc4af90c4075e5fdd6898869a143b0b07a355
pox <pox@pox>
2017-01-18 11:53:31 +0900

    aa

--- TeamProject/src/main/java/org/team/service/ImgServiceImpl.java
+++ TeamProject/src/main/java/org/team/service/ImgServiceImpl.java
@@ -11,34 +11,34 @@ public class ImgServiceImpl implements ImgService {
 
 	@Autowired
 	private ImgService service;
-	
+
 	@Override
 	public void create(ImgVO vo) throws Exception {
-		
+
 		service.create(vo);
 	}
 
 	@Override
 	public ImgVO read(Integer ino) throws Exception {
-		
+
 		return service.read(ino);
 	}
 
 	@Override
 	public void update(ImgVO vo) throws Exception {
-		
+
 		service.update(vo);
 	}
 
 	@Override
 	public void delete(Integer ino) throws Exception {
-		
+
 		service.delete(ino);
 	}
 
 	@Override
 	public List<ImgVO> list() throws Exception {
-		
+
 		return service.list();
 	}
 
--- TeamProject/src/test/java/org/team/web/MemberDAOTest.java
+++ TeamProject/src/test/java/org/team/web/MemberDAOTest.java
@@ -2,8 +2,6 @@ package org.team.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,55 +11,55 @@ import org.team.persistence.MemberDAOImpl;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 public class MemberDAOTest {
-	
+
 	@Autowired
 	private MemberDAOImpl dao;
-	
+
 	@Test
 	public void testCreate() throws Exception {
-		
+
 		MemberVO vo = new MemberVO();
 		vo.setUserid("user10");
 		vo.setUserpw("user10");
 		vo.setEmail("aaa@bbb.com");
 		vo.setUname("user10");
-		
+
 		dao.create(vo);
 		System.out.println(vo);
-		
+
 	}
-	
+
 	@Test
 	public void testRead() throws Exception {
-		
+
 		String userid = "user00";
 		System.out.println(dao.read(userid));
 	}
-	
+
 	@Test
 	public void testUpdate() throws Exception {
-		
+
 		MemberVO vo = new MemberVO();
 		vo.setUserpw("user11");
 		vo.setEmail("bbb@ccc.com");
 		vo.setUname("user11");
 		vo.setUserid("user00");
-		
+
 		dao.update(vo);
 		System.out.println(vo);
 	}
-	
+
 	@Test
 	public void testDelete() throws Exception {
-		
+
 		String userid = "user10";
 		dao.delete(userid);
 		System.out.println(userid);
 	}
-	
+
 	@Test
 	public void testList() throws Exception {
-		
+
 		System.out.println(dao.list());
 	}
 }
