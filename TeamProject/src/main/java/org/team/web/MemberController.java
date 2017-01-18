840ec0a8b424fedd4d814389f814a0b1b30fc17c
pox <pox@pox>
2017-01-18 18:06:17 +0900

    로그인 처리 다함

--- TeamProject/src/main/java/org/team/web/MemberController.java
+++ TeamProject/src/main/java/org/team/web/MemberController.java
@@ -1,16 +1,18 @@
 package org.team.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.team.domain.MemberVO;
 import org.team.service.MemberServiceImpl;
+import org.team.util.loginUtil;
 
 @Controller
 @RequestMapping("/member/*")
@@ -22,9 +24,8 @@ public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/myPage", method = RequestMethod.GET)
-	public void registGET(Model model, @RequestParam("userid") String userid) throws Exception {
+	public void registGET() throws Exception {
 		logger.info("MEMBER MYPAGE............");
-		model.addAttribute("member", service.read(userid));
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -37,20 +38,24 @@ public class MemberController {
 	}
 
 	@RequestMapping(value = "/loginPOST", method = RequestMethod.POST)
-	public String loginPOST(MemberVO vo, RedirectAttributes rttr) throws Exception {
-		logger.info("아이디" + vo.getUserid());
-		logger.info("비밀번호" + vo.getUserpw());
+	public String loginPOST(HttpServletRequest req, HttpServletResponse res, MemberVO vo, RedirectAttributes rttr)
+			throws Exception {
+		String userid = vo.getUserid();
+		String userpw = vo.getUserpw();
 		boolean check = service.memberLogin(vo);
-		logger.info("아이디 있는지 여부.." + check);
 		if (check == true) {
 			rttr.addFlashAttribute("msg", "loginSUCCESS");
 			logger.info("로그인성공..." + check);
-			return "redirect:/member/mypage";
+			return loginUtil.Success(req, res, userid, userpw);
 		} else {
 			rttr.addFlashAttribute("msg", "loginFail");
 			logger.info("로그인실패..." + check);
 			return "redirect:/index";
 		}
+	}
+
+	@RequestMapping(value = "/loginSUCCESS", method = RequestMethod.POST)
+	public void loginSUCCESS() throws Exception {
 	}
 
 	@RequestMapping(value = "/dupleCheck", method = RequestMethod.POST)
