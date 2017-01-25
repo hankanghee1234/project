c3567425e99f1c268700f85c19ac295a6fabb8ee
ss <ss@hankanghee>
2017-01-25 18:42:16 +0900

    File Controller

---
+++ TeamProject/src/main/java/org/team/web/FileController.java
@@ -0,0 +1,11 @@
+package org.team.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FileController {
+
+	 private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+}
