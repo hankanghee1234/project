ccd68c6e01a4916cd1fb47e1742c92e74386bb2f
ss <ss@hankanghee>
2017-01-25 17:19:18 +0900

    원복

--- TeamProject/src/main/java/org/team/service/ImgServiceImpl.java
+++ TeamProject/src/main/java/org/team/service/ImgServiceImpl.java
@@ -1,10 +1,14 @@
 package org.team.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.team.domain.ImgVO;
+import org.team.domain.PptVO;
 import org.team.persistence.ImgDAOImpl;
 
 @Service
@@ -18,10 +22,11 @@ public class ImgServiceImpl implements ImgService {
 
 		imgDAO.create(vo);
 	}
-
+	
+	@Transactional
 	@Override
 	public List<ImgVO> read(Integer fno) throws Exception {
-
+		
 		return imgDAO.read(fno);
 	}
 
