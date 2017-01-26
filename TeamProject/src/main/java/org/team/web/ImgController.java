000c693f5abd7754bba92f70a4fb4f912efa0d98
ss <ss@hankanghee>
2017-01-26 09:13:57 +0900

    img

--- TeamProject/src/main/java/org/team/web/ImgController.java
+++ TeamProject/src/main/java/org/team/web/ImgController.java
@@ -1,15 +1,22 @@
 package org.team.web;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.team.domain.ImgVO;
 import org.team.service.ImgServiceImpl;
 
+@CrossOrigin(origins = "*")
 @Controller
+@RequestMapping("/member/*")
 public class ImgController {
 
 	@Autowired
@@ -17,11 +24,19 @@ public class ImgController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ImgController.class);
 	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@RequestMapping(value = "/myPage", method = RequestMethod.GET)
 	public void imgView(Integer fno, Model model) throws Exception {
 		logger.info("Image View GET.............");
 		
 		model.addAttribute("pptLead", imgService.pptRead(fno));
 		model.addAttribute("imgRead", imgService.imgRead(fno));
+		
+	}
+	
+	@RequestMapping(value = "/imgRead", method = RequestMethod.GET)
+	public @ResponseBody List<ImgVO> readGET(Integer fno) throws Exception {
+		logger.info("read POST.............");
+
+		return imgService.imgRead(fno);
 	}
 }
