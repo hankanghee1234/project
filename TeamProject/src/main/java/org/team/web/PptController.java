32546bf61b9fca88f0befe39d598e20fe68d7b21
ss <ss@hankanghee>
2017-02-01 10:48:14 +0900

    controller 및 mapper 수정

--- TeamProject/src/main/java/org/team/web/PptController.java
+++ TeamProject/src/main/java/org/team/web/PptController.java
@@ -1,11 +1,16 @@
 package org.team.web;
 
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.IOUtils;
+import org.imgscalr.Scalr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.team.domain.FileVO;
 import org.team.domain.ImgVO;
 import org.team.domain.PptVO;
@@ -50,10 +56,30 @@ public class PptController {
 	@ResponseBody
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public String upload(@RequestBody MultipartFile file) throws Exception {
-		logger.info("Dropzone POST......" + file);
+		logger.info("drag & drop POST......" + file);
 		
 		UUID uid = UUID.randomUUID();
-		return "Success";
+		
+		InputStream is = file.getInputStream();
+		String fileName = file.getOriginalFilename();
+
+		String uploadName = uid + "_" + fileName;
+		
+		FileOutputStream fos = new FileOutputStream("C:\\zzz\\" + fileName); 
+		FileOutputStream foss = new FileOutputStream("C:\\zzz\\" + uploadName); 
+		
+		BufferedImage origin = ImageIO.read(is);
+		
+		BufferedImage destImg = Scalr.resize(origin,
+				Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100); 
+		
+		ImageIO.write(origin, "jpg", fos);
+		ImageIO.write(destImg, "jpg", foss);
+		
+		fos.close();
+		foss.close();
+		
+		return uploadName;
 	}
 	
 	@RequestMapping(value = "/chatList", method = RequestMethod.GET)
@@ -83,11 +109,21 @@ public class PptController {
 		return imgService.imgRead(fno);
 	}
 	
+	@RequestMapping(value = "/pptCreate", method = RequestMethod.GET)
+	public void createGET() throws Exception {
+		logger.info("pptCreate GET............");
+	}
+	
 	@RequestMapping(value = "/pptCreate", method = RequestMethod.POST)
-	public String create(FileVO fvo, ImgVO ivo, PptVO pvo) throws Exception {
+	public String create(FileVO fvo, ImgVO ivo, PptVO pvo, RedirectAttributes rttr) throws Exception {
 		logger.info("pptCreate POST............");
+		logger.info(fvo.toString());
+		logger.info(ivo.toString());
+		logger.info(pvo.toString());
 		
-		fvo.
-		return "";
+		imgService.create(ivo, fvo, pvo);
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:./myPage";
 	}
 }
